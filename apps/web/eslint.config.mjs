573cc3c814f8d203ea1c15bf37a2373bc7140eeb
p <p@play.link>
2026-01-31 12:21:52 +0100

    chore: update example env vars and eslint config [YOLO]

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -10,6 +10,7 @@ export default antfu({
     'react-hooks/exhaustive-deps: ["warn"]': 'off',
     'react/no-children-to-array': 'off',
     'react-hooks/refs': 'off',
+    'react-refresh/only-export-components': ['error', {allowExportNames: ['meta', 'loader', 'action', 'headers']}],
     'ts/consistent-type-definitions': 'off',
     'ts/no-use-before-define': 'off',
   },
